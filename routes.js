d41daecd2680b32576683bdffa80b7a94e1bf9f2
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-11-05 19:12:18 -0600

    Added Route Mapping

--- routes.js
+++ routes.js
@@ -1,3 +1,8 @@
 const routes = require('next-routes')();
 
+routes
+.add('/campaigns/new','/campaigns/new')
+.add('/campaigns/:address','/campaigns/show');//":" shows that what goes next address" is a wild card
+
+
 module.exports = routes;
