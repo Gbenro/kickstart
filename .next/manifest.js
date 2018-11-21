60e2a97f0c3e916b8b3af419cc01cbca48d4f795
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-11-20 22:40:25 -0600

    Added comments to solidity code, campaign.sol

--- .next/manifest.js
+++ .next/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "ea602a93501bf7a9c104"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "6848f3f941a1a55d31e8"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_error.js","5":"bundles\\pages\\campaigns\\requests.js","7":"bundles\\pages\\campaigns\\requests\\new.js"}[chunkId]||chunkId) + "-" + {"0":"e8a7490df19a875e5dbb","1":"cb143352083992e4723a","5":"fbad8413c89dbbfa35cc","7":"51b44a3059ea8327429a"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_error.js","3":"bundles\\pages\\index.js","4":"bundles\\pages\\campaigns\\show.js"}[chunkId]||chunkId) + "-" + {"0":"e8a7490df19a875e5dbb","1":"cb143352083992e4723a","3":"f114555da4867feabaf4","4":"a68dad0c03a91174c95c"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,14 +1,22 @@
 pragma solidity ^0.4.20;
 
-
+/// @title Kickstart factory contract
+/// @author Gbenro Adesoye
+/// @notice A factory contract for the main Campaign contract
 contract CampaignFactory {
-    address [] public deployedCampaigns;
+    address[] public deployedCampaigns;
 
+  
+    /// @notice This function creates campaign by calling constructor of campaign
+    /// @param minimum The minimun amount that can be donated to this campaign
     function createCampaign (uint minimum) public{
-      address newCampaign =   new Campaign (minimum, msg.sender);
-      deployedCampaigns.push(newCampaign);
+        address newCampaign = new Campaign (minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
     }
 
+
+    /// @notice gets the addresses that are already deployed
+    /// @return the array of addresses that is deployed by this contract
     function getDeployedCampaigns() public view returns (address[]){
 
         return deployedCampaigns;
@@ -18,7 +26,9 @@ contract CampaignFactory {
 }
 
 
-
+///@title Kickstart
+///@author Gbenro Adesoye
+///@notice A Dapp version for Kickstarter. Learnt through Ethereum course on Udemy
 contract Campaign{
     struct Request{
         string description;
@@ -29,56 +39,70 @@ contract Campaign{
         mapping (address => bool) approvals;// people that approves the Request
     }
 
-      address public manager;
-      uint public minContribution;
-      mapping(address => bool) public approvers;
-      Request[] public requests;
-      uint public approversCount;
+    address public manager;
+    uint public minContribution;
+    mapping(address => bool) public approvers;
+    Request[] public requests;
+    uint public approversCount;
 
-
-       modifier restricted(){
+    ///@notice to restict access to only address that is manager of campaign
+    modifier restricted(){
         require(msg.sender == manager);
         _;
 
     }
 
+    ///@notice contructor to campaign contract
+    ///@param minimum the minimum amount of ether that can be contributed to the campaign
+    ///@param creator the address of the manager of the campaign
     constructor (uint minimum, address creator) public{
-       manager = creator;
-       minContribution= minimum;
+        manager = creator;
+        minContribution = minimum;
 
     }
 
+    ///@notice payable funtion, contribute ether to the campaign
     function contribute() public payable{
         require(msg.value > minContribution);// msg.value is the amount in wei
-       approvers[msg.sender]= true;//adds a new key of the address and gives ita mapping of true
+        approvers[msg.sender] = true;//adds a new key of the address and gives ita mapping of true
         approversCount++;
     }
 
-
+    ///@notice creates request for money in the campaign, maybe manager needs to buy something need for project
+    ///@dev should only be called by the manager
+    ///@param description the purpose of the request
+    ///@param value amount of ether requested for
+    ///@param address the address of the recipient of the requested fund, should be third part ideally
     function createRequest(string description, uint value, address recipient) public restricted {
-      Request memory newRequest = Request ({
-          description:description,
-          value:value,
-          recipient:recipient,
-          complete: false,
-          approvalCount: 0
+        Request memory newRequest = Request ({
+            description:description,
+            value:value,
+            recipient:recipient,
+            complete: false,
+            approvalCount: 0
 
-      }) ;
+        }) ;
      // Request(description,value,recipient, false); this is another way to initialize a struct
-      requests.push(newRequest);
+        requests.push(newRequest);
     }
-
+    ///@notice approvers i.e people that has contributed to this campaign can should to approve request or not
+    ///@dev request must point to the storage request
+    ///@param index the index of request in the array of request for a campaign
     function approveRequest (uint index) public{
-    Request storage request = requests[index];
-        require(approvers[msg.sender]);// checking if he is a contributor
+        Request storage request = requests[index];
+        require(approvers[msg.sender]);// checking if he/she is a contributor
         require(!request.approvals[msg.sender]);// if they havent voted on this before
 
-        request.approvals[msg.sender]= true;
+        request.approvals[msg.sender] = true;
         request.approvalCount++;
 
     }
+
+    ///@notice finalize request if more than 50% of contributors approve request, restricted to only campaign manager
+    ///@dev request must point to the storage request
+    ///@param index the index of request in the array of request for a campaign
     function finalizeRequest(uint index) public restricted{
-         Request storage request = requests[index];
+        Request storage request = requests[index];// must point to the request in the storage
         require(!request.complete);
         require(request.approvalCount > (approversCount/2));
 
@@ -86,7 +110,11 @@ contract Campaign{
         request.complete = true;
 
     }
-
+    ///@notice show all info about a campaign
+    ///@return uint the minimum contribution for the campaign
+    ///@return uint the balance of the campaign
+    ///@return uint the numbers of requests in the campaign
+    ///@return address campaign's manager address
     function getSummary() public view returns (
         uint, uint, uint, uint, address
       ){
@@ -94,14 +122,15 @@ contract Campaign{
         return (
 
             minContribution,
-            this.balance,
+            address(this).balance,
             requests.length,
             approversCount,
             manager
         );
     }
 
-
+    ///@notice gets th number of request made by manager to the campaign
+    ///@return the amount of request for a campaign
     function getRequestsCount() public view returns (uint){
 
         return requests.length;
